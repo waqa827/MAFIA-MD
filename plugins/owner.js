ea71936b12784a1500514dd1438c77296ff7f7f5
MAFIAADEEL <mafiaadeel302@gmail.com>
2025-05-30 06:36:30 +0500

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -29,7 +29,7 @@ async (conn, mek, m, { from }) => {
 
         // Send the owner contact message with image and audio
         await conn.sendMessage(from, {
-            image: { url: 'https://files.catbox.moe/149k8x.jpg' }, // Image URL from your request
+            image: { url: 'https://files.catbox.moe/fbpv5l.jpg' }, // Image URL from your request
             caption: `╭━━〔 *MAFIA-MD* 〕━━┈⊷
 ┃◈╭─────────────·๏
 ┃◈┃• *Here is the owner details*
@@ -45,7 +45,7 @@ async (conn, mek, m, { from }) => {
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '120363300351654980@newsletter',
-                    newsletterName: '𝙼𝙰𝙵𝙸𝙰 𝙰𝙳𝙴𝙴𝙻',
+                    newsletterName: '𐌼𐀁ҒƖ𐀁 𐀁ƉЄЄ˩🤖',
                     serverMessageId: 143
                 }            
             }
@@ -53,7 +53,7 @@ async (conn, mek, m, { from }) => {
 
         // Send audio as per your request
         await conn.sendMessage(from, {
-            audio: { url: 'https://github.com/JawadYT36/KHAN-DATA/raw/refs/heads/main/autovoice/menunew.m4a' }, // Audio URL
+            audio: { url: 'https://files.catbox.moe/4fz6jh.mp3' }, // Audio URL
             mimetype: 'audio/mp4',
             ptt: true
         }, { quoted: mek });
