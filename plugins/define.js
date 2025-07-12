7e0c55d03e2884d261582797df0e869133edd93f
MAFIA-ADEEL <mafiaadeel302@gmail.com>
2025-07-12 15:20:55 +0500

    Update define.js

--- plugins/define.js
+++ plugins/define.js
@@ -31,7 +31,7 @@ async (conn, mek, m, { from, q, reply }) => {
 ✍️ *Example*: ${example}  
 📝 *Synonyms*: ${synonyms}  
 
-🔗 *Powered By Jawad Tech X*`;
+🔗 *ᴘᴏᴡᴇʀᴅ ʙʏ ᴍᴀғɪᴀ ᴀᴅᴇᴇʟ*`;
 
         if (audio) {
             await conn.sendMessage(from, { audio: { url: audio }, mimetype: 'audio/mpeg' }, { quoted: mek });
