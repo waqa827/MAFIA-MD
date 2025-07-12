c002b5560afa490446d62cd4c868c4d4b2e057b1
MAFIA-ADEEL <mafiaadeel302@gmail.com>
2025-07-12 08:54:43 +0500

    Update menu-new.js

--- plugins/menu-new.js
+++ plugins/menu-new.js
@@ -39,7 +39,7 @@ cmd({
             forwardingScore: 999,
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
-                newsletterJid: '120363300351654980@newsletter',
+                newsletterJid: '120363401572096965@newsletter',
                 newsletterName: config.OWNER_NAME,
                 serverMessageId: 143
             }
